963052ea29897a51fe770390cd608734d813d877
AnugetHub1627 <anummore27@gmail.com>
2026-08-12 00:29:34 -0700

    Define outputs for EC2 and EKS resources
    Add outputs for EC2 public IP and EKS cluster endpoint.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,12 @@
+output "instance_public_ip" {
+  description = "Public IP address of the deployed EC2 server"
+  value       = aws_instance.ci-cd_host.public_ip
+}
+output "sonarqube_server_ip" {
+  value = aws_instance.ci-cd_host.public_ip
+}
+
+output "eks_cluster_endpoint" {
+  value = aws_eks_cluster.ci-cd-EKS.endpoint
+}
 
