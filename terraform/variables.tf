222abd774761c3896892f7707e7ebe587dd562e5
AnugetHub1627 <anummore27@gmail.com>
2026-08-12 00:29:58 -0700

    Add VPC and subnet variables to Terraform config

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,62 @@
+variable "vpc_cidr" {
+    type = string 
+    default = "10.0.0.0/16"
+}
+variable "vpc_name" {
+    type = string
+    default = "ci-cd-vpc"
+}
+variable "igw_name" {
+    type = string
+    default = "ci-cd-igw"
+}
+variable "subpub1a_cidr" {
+    type = string
+    default = "10.0.1.0/24"
+}
+variable "subpub1a_name" {
+    type = string
+    default = "ci-cd-pub1a"
+}
+variable "subpub1b_cidr" {
+    type = string
+    default = "10.0.2.0/24"
+}
+variable "subpub1b_name" {
+    type = string
+    default = "ci-cd-pub1a"
+}
+variable "subpvt1a_cidr" {
+    type = string
+    default = "10.0.3.0/24"
+}
+variable "subpvt1a_name" {
+    type = string
+    default = "ci-cd-pvt1a"
+}
+variable "subpvt1b_cidr" {
+    type = string
+    default = "10.0.4.0/24"
+}
+variable "subpvt1b_name" {
+    type = string
+    default = "ci-cd-pvt1b"
+}
+variable "nat_name" {
+    type = string
+    default = "ci-cd-nat"
+}
+variable "cicd_host_ami" {
+    type = string
+    default = "ami-01a00762f46d584a1"
+}
+variable "ci-cd-ec2-type" {
+    type = string 
+    default = "m7i-flex.large"
+}
+variable "key_name" {
+    type = string
+    default = "key_mukesh"
+}
+
 
