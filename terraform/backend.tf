ff00b07f8a50cbde2b80dd528b94ac08ab687bfe
AnugetHub1627 <anummore27@gmail.com>
2026-08-14 02:10:01 -0700

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "anu-ci-cd-terraform-state-bucket"
-    key            = "devops-e2e/terraform.tfstate"
-    region         = "ap-south-1" # Use your targeted region
-    encrypt        = true
-    # Optional but highly recommended: DynamoDB for state locking
-    # dynamodb_table = "ci-cd-terraform-lock-table" 
+    bucket = "anu-ci-cd-terraform-state-bucket"
+    key    = "state/terraform.tfstate"
+    region = "ap-south-1"
   }
 }
