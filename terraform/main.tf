b5212b998b0bd4887258428a6289eada0a6fa10f
AnugetHub1627 <anummore27@gmail.com>
2026-08-14 02:12:43 -0700

    Fix Principal service in assume_role_policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -211,9 +211,9 @@ resource "aws_iam_role" "eks_cluster_role" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = { Service = "eks.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "://amazonaws.com" }
     }]
   })
 }
@@ -248,9 +248,9 @@ resource "aws_iam_role" "eks_node_role" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = { Service = "ec2.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "://amazonaws.com" }
     }]
   })
 }
