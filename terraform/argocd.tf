131af004681a322f46f2c02159ed13760f595716
AnugetHub1627 <anummore27@gmail.com>
2026-08-18 23:25:34 -0700

    Create argocd.tf

---
+++ terraform/argocd.tf
@@ -0,0 +1,32 @@
+# Add helm and kubernetes provider 
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+# Hardcoded exact cluster name here so Terraform can look it up in AWS
+data "aws_eks_cluster" "cluster" {
+  name = "ci-cd-EKS" 
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = "ci-cd-EKS"
+}
+
+# 3. Deploy the Argo CD Helm release
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://github.io"
+  chart            = "argo-cd"
+  version          = "7.3.11"
+  namespace        = "argocd"
+  create_namespace = true
+
+  set {
+    name  = "server.service.type"
+    value = "ClusterIP"
+  }
+}
