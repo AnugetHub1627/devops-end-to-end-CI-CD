5860bfb038b709d87ddbd04c185aa0a383b93cfb
AnugetHub1627 <anummore27@gmail.com>
2026-08-19 03:24:29 -0700

    Update argocd.tf

--- terraform/argocd.tf
+++ terraform/argocd.tf
@@ -1,28 +1,19 @@
 provider "helm" {
   kubernetes = {
-    # FIXED: Replaced data block paths with direct resource dependencies
-    host                   = aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
 
-
-# Hardcoded exact cluster name here so Terraform can look it up in AWS
-data "aws_eks_cluster_auth" "cluster" {
-  name = aws_eks_cluster.this.name
+# Safely lookup EKS parameters using the cluster's explicit name string
+data "aws_eks_cluster" "cluster" {
+  name = "ci-cd-EKS"
 }
-
-# Deploy the Argo CD Helm release
-resource "helm_release" "argocd" {
-  name             = "argocd"
-  repository       = "https://github.com/AnugetHub1627/devops-end-to-end-CI-CD.git"
-  chart            = "argo-cd"
-  version          = "7.3.11"
-  namespace        = "argocd"
-  create_namespace = true
-
-  depends_on = [
+data "aws_eks_cluster_auth" "cluster" {
+  name = "ci-cd-EKS"
+}
+depends_on = [
     data.aws_eks_cluster.cluster,
     data.aws_eks_cluster_auth.cluster
   ]
