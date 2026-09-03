ec07291cb7e32a49864aa00a31fad79f6ff2041a
AnugetHub1627 <anummore27@gmail.com>
2026-09-03 02:41:50 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,6 +23,7 @@ terraform {
 resource "aws_vpc" "ci-cd-vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
+  enable_dns_support   = true # FIX: Mandatory for EKS control-to-data-plane registration
 
   tags = {
     Name                                       = var.vpc_name
