5f7b06c95ee92e3788af1d5f7ce4f83621cbe345
AnugetHub1627 <anummore27@gmail.com>
2026-09-02 23:27:15 -0700

    Fix EKS cluster references in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -316,15 +316,15 @@ data "aws_eks_cluster" "eks" {
   name = "ci-cd-EKS"
 }
 data "aws_eks_cluster_auth" "eks" {
-  name = data.aws_eks_cluster.eks.name
+  name = data.aws_eks_cluster.ci-cd-EKS.name
 }
 # ==========================================
 # 3. HELM PROVIDER BLOCK
 # ==========================================
 provider "helm" {
   kubernetes {
-    host                   = aws_eks_cluster.eks.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
+    host                   = aws_eks_cluster.ci-cd-EKS.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.ci-cd-EKS.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.eks.token
   }
 }
